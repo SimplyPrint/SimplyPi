ca2ca94a1d829b6aec4fba3ae2c17ca4e7f03840
jneilliii <jneilliii+github@gmail.com>
2021-12-11 16:06:49 -0500

    fallback if model is not in mapping dictionary

--- scripts/files/root/usr/lib/raspiwifi/reset_device/reset.py
+++ scripts/files/root/usr/lib/raspiwifi/reset_device/reset.py
@@ -33,7 +33,7 @@ else:
 
 model_info_match = re.search(r"Model\s+:\s(.+)$", cpuinfo, re.MULTILINE)
 if model_info_match:
-    model_info = pi_map[model_info_match.group(1).strip()]
+    model_info = pi_map[model_info_match.group(1).strip()] or 'unknown'
 else:
     model_info = 'unknown'
 
